8cd911f62e6ddaf38f64d7b627b14b1fa272538b
Akari <157141929+Akari0desu@users.noreply.github.com>
2025-02-14 15:09:39 +0000

    Add files via upload
    This one is the refactorized one with Class and more mathematical operations and more comments

--- Calculator.py
+++ Calculator.py
@@ -7,61 +7,92 @@ Created on Wed Feb 12 14:01:27 2025
 #importing math for the use of mathimatical functions
 import math
 
+class Calculator:
+
 #function for addition
-def add(a, b):
+  def add(a, b):
     return a + b
 
 #function for substraction
-def subtract(a, b):
+  def subtract(a, b):
     return a - b
 
 #function for multiplying
-def multiply(a, b):
+  def multiply(a, b):
     return a * b
 
 #function for deviding
-def divide(a, b):
+  def divide(a, b):
     if b == 0:
         return "Error: Division by zero"
     return a / b
 
 #function for sine calculation
-def sine(a):
+  def sine(a):
     return math.sin(math.radians(a))
 
 #fucntion for cosine calculation
-def cosine(a):
+  def cosine(a):
     return math.cos(math.radians(a))
 
 #function for tangent calculation
-def tangent(a):
+  def tangent(a):
     return math.tan(math.radians(a))
 
 #function for arcsin calculation, it is inverse of sine
-def arcsin(a):
+  def arcsin(a):
     if -1 <= a <= 1:
         return math.degrees(math.asin(a))
-    return "Error: Input not between"
+    return "Error: Input out of range (-1 to 1)"
 
 #function for arccos calculation, it is inverse of cos
-def arccos(a):
+  def arccos(a):
     if -1 <= a <= 1:
         return math.degrees(math.acos(a))
-    return "Error: Input not between"
+    return "Error: Input out of range (-1 to 1)"
 
 #function for arctan calculation, it is inverse of tangent
-def arctan(a):
+  def arctan(a):
     return math.degrees(math.atan(a))
 
 #function for square of a number
-def square(a):
+  def square(a):
     return a ** 2
 
 #function for the square root of a number
-def square_root(a):
+  def square_root(a):
     if a < 0:
-        return "Error: Cannot calculate a square root of negative number"
+        return "Error: Cannot calculate the root of negative number"
     return math.sqrt(a)
+
+# to calculate logarithm base 10
+  def logarithm (a):
+    if a<= 0 :
+        return "Error: cannot be zero or negative"
+    return math.log(a)
+
+#to find natural log base e
+  def natural_log(a):
+    if a <= 0:
+        return "Error: cannot be zero or negative"
+    return math.log(a)
+
+#to find the cube of a number
+  def cube(a):
+    return a ** 3 
+
+#to find cube root of a number
+  def cube_root(a):
+    return a **(1/3)
+
+            
+
+
+
+
+
+
+
 
 
 
